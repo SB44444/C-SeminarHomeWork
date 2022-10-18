7b6fe469641fb50105f8e136aa982e9cf9041cb0
SB44444 <mega.s.style@gmail.com>
2022-10-18 23:18:45 +0300

    Tesk 2

--- C#SeminarHWork1/Program.cs
+++ C#SeminarHWork1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input Number1");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Number2");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+if(Number1 == Number2)
+    Console.WriteLine($"Number {Number1} is equally Number {Number2}");   
+else if(Number1 > Number2)
+    Console.WriteLine($"Number {Number1} is upper Number {Number2}");
+else
+    Console.WriteLine($"Number {Number2} is upper Number {Number1}");
