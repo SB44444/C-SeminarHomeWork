441d1539dbc2c0d441bad1ec93d7e3e4089f492b
SB44444 <mega.s.style@gmail.com>
2022-10-19 02:56:16 +0300

    Tesk 8

--- C#SeminarHWork1/Program.cs
+++ C#SeminarHWork1/Program.cs
@@ -13,7 +13,9 @@ else if(Number1 > Number2)
 else
     Console.WriteLine($"Number {Number2} is upper Number {Number1}"); */
 
+
 /* Семинар 1  Задача № 4  программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел */
+
 /*Console.WriteLine("Input Number1");
 int Number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input Number2");
@@ -27,12 +29,24 @@ if (NumberMax < Number3)
     NumberMax = Number3;
 Console.WriteLine($"The maximunm dight is Number {NumberMax}");*/
 
+
 /* Семинар 1  Задача № 6: Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)*/
 
-Console.WriteLine("Input Number");
+/*Console.WriteLine("Input Number");
 int Number = Convert.ToInt32(Console.ReadLine());
 if(Number % 2 == 0)
     Console.WriteLine($"Digit {Number} is even");   
 else 
-    Console.WriteLine($"Digit {Number} is even odd");
+    Console.WriteLine($"Digit {Number} is even odd");*/
 
+/* Семинар 1  Задача №  8: Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+/*Console.WriteLine("Input Number upper 1");
+int Number = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current <= Number)
+   {
+    if(current % 2 == 0)
+        Console.Write(current + ", ");
+        current++;
+   }*/
